da2acca746bd6c3f7b6bbfcd5fd2a01d38c70a5a
Arno Leruste <recroX91@hotmail.com>
2023-12-10 15:41:29 +0100

    work on pop up when deleting member

---
+++ src/app/pages/confirm-dialog/confirm-dialog.component.ts
@@ -0,0 +1,27 @@
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+
+
+@Component({
+  selector: 'app-confirm-dialog',
+  standalone: true,
+  imports: [],
+  templateUrl: './confirm-dialog.component.html',
+  styleUrl: './confirm-dialog.component.scss'
+})
+
+export class ConfirmDialogComponent {
+
+  constructor(
+    public dialogRef: MatDialogRef<ConfirmDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) {}
+
+  onConfirm(): void {
+    this.dialogRef.close(true); // Retourne true pour confirmer la suppression
+  }
+
+  onClose(): void {
+    this.dialogRef.close(false); // Retourne false pour annuler la suppression
+  }
+}
--- src/app/pages/members/members.component.ts
+++ src/app/pages/members/members.component.ts
@@ -3,6 +3,8 @@ import { Component, OnInit } from '@angular/core';
 import { MemberService } from '../../services/memberService';
 import { Member } from '../../models/member';
 import { Router, RouterLink } from '@angular/router';
+import { ConfirmDialogComponent } from '../confirm-dialog/confirm-dialog.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-members',
@@ -15,7 +17,10 @@ import { Router, RouterLink } from '@angular/router';
 export class MembersComponent implements OnInit {
   members: Member[] = [];
 
-  constructor(private memberService: MemberService, private router: Router) {}
+  constructor(
+    private memberService: MemberService, 
+    private router: Router,
+    private dialog: MatDialog) {}
 
   ngOnInit(): void {
     this.refreshMembers();
@@ -35,7 +40,7 @@ export class MembersComponent implements OnInit {
     // Logique pour l'édition du membre
   }
 
-  deleteMember(member: Member) {
+  deleteMember(member:Member){
     this.memberService.deleteMember(member.id).subscribe(() => {
       this.refreshMembers();
     });
@@ -43,5 +48,31 @@ export class MembersComponent implements OnInit {
 
   showDetails(member: Member) {
     this.router.navigate(['/member', member.id]);
+  }
+
+  confirmDelete(member: Member) {
+    this.confirmPopUp(member);
+  }
+
+  private confirmPopUp(member: Member) {
+    // const dialogRef = this.dialog.open(ConfirmDialogComponent, {
+    //   width: '250px',
+    //   data: { memberData: member }, // Vous pouvez passer des données à la boîte de dialogue si nécessaire
+    //   panelClass: 'custom-dialog-container',
+    //   disableClose: true, // Empêche la fermeture de la boîte de dialogue par clic à l'extérieur
+    //   backdropClass: 'custom-backdrop' // Ajoutez une classe pour personnaliser l'arrière-plan de la boîte de dialogue
+    // });
+
+    // dialogRef.afterClosed().subscribe(result => {
+    //   if (result) {
+    //     // Si l'utilisateur a confirmé la suppression
+    //     this.deleteMember(member);
+    //   }
+    // });
+
+    const confirmed : boolean = confirm('Êtes-vous sûr de vouloir supprimer ce membre ?');
+    if (confirmed) {
+      this.deleteMember(member);
+    }
   }
 }
