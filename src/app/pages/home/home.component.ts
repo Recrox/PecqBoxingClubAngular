aa3c19c9281dd6f7e4721b854169aad398f5369d
Arno Leruste <recroX91@hotmail.com>
2023-12-10 14:49:20 +0100

    create button style and delete main component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { WeatherComponent } from '../weather/weather.component';
-import { MainComponent } from '../main/main.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [WeatherComponent,MainComponent],
+  imports: [WeatherComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
